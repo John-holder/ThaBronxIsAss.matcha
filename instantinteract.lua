883545104ca7c63426324e44ef28b664e051d5ec
John-holder <businessseattlebreno@gmail.com>
2026-04-03 14:14:44 -0700

    Optimize

--- instantinteract.lua
+++ instantinteract.lua
@@ -1,4 +1,4 @@
--- Made by 938036904912379924 @ Matcha
+-- Made by 938036904912379924 @ Matcha. Optimized for better usage.
 -- https://discord.com/channels/1420041567481364703/1489127011988541521
 local HOLD_OFFSET = 0x140
 local DIST_OFFSET = 0x148
@@ -6,25 +6,26 @@ local PATCH_DISTANCE = 25
 local patchedAddresses = {}
 local function patchPrompt(obj)
     local addr = obj.Address
-    if addr and addr ~= 0 and not patchedAddresses[addr] then
-        pcall(function()
-            memory_write("float", addr + HOLD_OFFSET, 0.0)
-            memory_write("float", addr + DIST_OFFSET, PATCH_DISTANCE)
-            patchedAddresses[addr] = true
-        end)
+    if not addr or addr == 0 or patchedAddresses[addr] then
+        return
+    end
+    local s, e = pcall(function()
+        memory_write("float", addr + HOLD_OFFSET, 0.0)
+        memory_write("float", addr + DIST_OFFSET, PATCH_DISTANCE)
+    end)
+    if s then
+        patchedAddresses[addr] = true
     end
 end
-task.spawn(function()
-    while true do
-        pcall(function()
-            local descendants = workspace:GetDescendants()
-            for i = 1, #descendants do
-                local v = descendants[i]
-                if v.ClassName == "ProximityPrompt" then
-                    patchPrompt(v)
-                end
-            end
-        end)
-        task.wait(2)
+local descendants = workspace:GetDescendants()
+for i=1, #descendants do
+    local prompt = descendants[i]
+    if prompt.ClassName == "ProximityPrompt" then
+        patchPrompt(prompt)
+    end
+end
+workspace.DescendantAdded:Connect(function(prompt)
+    if prompt.ClassName == "ProximityPrompt" then
+        patchPrompt(prompt)
     end
 end)
