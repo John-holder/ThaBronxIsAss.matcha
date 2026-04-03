33ce3ad941270c62ec33b50100db9c56346ab3a8
John-holder <businessseattlebreno@gmail.com>
2026-04-03 13:40:44 -0700

    Change Instant Interact Method

--- instantinteract.lua
+++ instantinteract.lua
@@ -1,31 +1,30 @@
--- Made originally by 987402090680827914 (@Lost)
-local ok, res = pcall(function()
-    return game:HttpGet("https://offsets.ntgetwritewatch.workers.dev/offsets.json")
-end)
-
-local offset
-if ok then
-    local HttpService = game:GetService("HttpService")
-    local data = HttpService:JSONDecode(res)
-    offset = data and data.ProximityPromptHoldDuraction and tonumber(data.ProximityPromptHoldDuraction, 16)
+-- Made by 938036904912379924 @ Matcha
+-- https://discord.com/channels/1420041567481364703/1489127011988541521
+local HOLD_OFFSET = 0x140
+local DIST_OFFSET = 0x148
+local PATCH_DISTANCE = 25
+local patchedAddresses = {}
+local function patchPrompt(obj)
+    local addr = obj.Address
+    if addr and addr ~= 0 and not patchedAddresses[addr] then
+        pcall(function()
+            memory_write("float", addr + HOLD_OFFSET, 0.0)
+            memory_write("float", addr + DIST_OFFSET, PATCH_DISTANCE)
+            patchedAddresses[addr] = true
+        end)
+    end
 end
-
-if offset then    
-    task.spawn(function()
-        while true do
-            for _, prompt in ipairs(game:GetDescendants()) do
-                if prompt.ClassName == "ProximityPrompt" then
-                    pcall(function()
-                        local promptAddress = prompt.Address
-                        local targetAddress = promptAddress + offset
-                        memory_write("float", targetAddress, 0)
-                    end)
+task.spawn(function()
+    while true do
+        pcall(function()
+            local descendants = workspace:GetDescendants()
+            for i = 1, #descendants do
+                local v = descendants[i]
+                if v.ClassName == "ProximityPrompt" then
+                    patchPrompt(v)
                 end
             end
-            task.wait(1.5)
-        end
-    end)
-    print("InstantInteract Loaded")
-else
-    print("InstantInteract Error")
-end
+        end)
+        task.wait(2)
+    end
+end)
